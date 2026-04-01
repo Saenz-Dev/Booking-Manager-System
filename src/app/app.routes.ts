0092649e8cb7b830bb1641f95a4e6477d85a62b9
Saenz-Dev <miguel.saenz02@uptc.edu.co>
2026-04-01 07:53:26 -0500

    Agregué ruta de login, nos reunimos con Yeimi, panel Login - Encargada Yeimi

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Registrar } from './components/registrar/registrar';
+import { Login } from './components/login/login';
 
-export const routes: Routes = [{
-    path: 'registrar', component: Registrar
-}];
+export const routes: Routes = [
+    { path: 'registrar', component: Registrar },
+    { path: 'login', component: Login }
+];
