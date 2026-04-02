1b05c33eed211257ab6105f7e033e94a252dd6a3
Saenz-Dev <miguel.saenz02@uptc.edu.co>
2026-04-02 09:59:32 -0500

    Creación de panel de inicio - encargada - Jimena

---
+++ src/app/components/panel-inicio/panel-inicio.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-panel-inicio',
+  imports: [],
+  templateUrl: './panel-inicio.html',
+})
+export class PanelInicio {
+
+}
