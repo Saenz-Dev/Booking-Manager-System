56231d3e54485bd28685c876c181ac24a89959de
yeimirojas02-lgtm <yeimi.rojas02@uptc.edu.co>
2026-04-04 00:29:14 -0500

    Creacion login

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './login.html',
-  styleUrl: './login.css',
+  styleUrl: './login.css'
 })
 export class Login {
-  
+
+  email: string = "";
+  password: string = "";
+  error: string = "";
+
+  constructor(private router: Router) {}
+
+  iniciarSesion() {
+
+  if (!this.email || !this.password) {
+    this.error = "Todos los campos son obligatorios";
+    return;
+  }
+
+  if (this.email === "admin@admin.com" && this.password === "1234") {
+    this.router.navigate(['/inicio']);
+  } else {
+    this.error = "Credenciales incorrectas";
+  }
+
+}
+
 }
