cca9d5143985fa948c8dde4c0f60733e926d5c30
Danna <danna.lara@uptc.edu.co>
2026-04-04 11:21:22 -0500

    panel inicio}

--- src/app/components/panel-inicio/panel-inicio.ts
+++ src/app/components/panel-inicio/panel-inicio.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-panel-inicio',
+  standalone: true,
   imports: [],
   templateUrl: './panel-inicio.html',
+  styleUrls: ['./panel-inicio.css']
 })
 export class PanelInicio {
-
+ 
 }
